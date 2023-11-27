076263e1ec65eda8a47ca1b9728d53900c755dca
Réczi Zsolt <recze01@gmail.com>
2023-11-27 17:38:59 +0100

    Fontawesome hozzáadás

--- resources/views/ckeditor.blade.php
+++ resources/views/ckeditor.blade.php
@@ -11,6 +11,7 @@
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
     <link rel="stylesheet" href="{{ asset('vendor/file-manager/css/file-manager.css') }}">
 </head>
 <body>
